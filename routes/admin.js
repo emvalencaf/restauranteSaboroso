827ae6e83b05546be54a9e702c188d642c5423d7
emvalencaf <edsonmvf@gmail.com>
2022-10-07 15:55:51 -0300

    update ejs e dashboard da admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const { route } = require('.');
+const admin = require('../inc/admin.mysql');
 var router = express.Router();
 var users = require('../inc/users');
 
@@ -17,12 +17,32 @@ router.use(function(req, res, next){
 
     };
 
+});
+
+router.use(function(req , res, next){
+
+    req.menus = admin.getMenus(req);
+
+    next();
+
 })
 
 router.get('/', function(req, res, next){
 
-    res.render('admin/index', {
-    });
+    admin.dashboard()
+        .then(data =>{
+
+            res.render('admin/index', admin.getParams(req, {
+                data
+            }));
+
+        })
+        .catch(err => {
+
+            console.error(err);
+
+        });
+
 
 });
 
@@ -72,41 +92,33 @@ router.post('/login', function(req, res, next){
 
 router.get('/contacts', function(req, res, next){
 
-    res.render('admin/contacts', {
-
-    });
+    res.render('admin/contacts', admin.getParams(req));
 
 });
 
 router.get('/emails', function(req, res, next){
 
-    res.render('admin/emails', {
-
-    });
+    res.render('admin/emails', admin.getParams(req));
 
 });
 
 router.get('/menus', function(req, res, next){
 
-    res.render('admin/menus', {
-
-    });
+    res.render('admin/menus', admin.getParams(req));
 
 });
 
 router.get('/reservations', function(req, res, next){
 
-    res.render('admin/reservations', {
-        date:{}
-    });
+    res.render('admin/reservations', admin.getParams(req, {
+        date: {}
+    }));
 
 });
 
 router.get('/users', function(req, res, next){
 
-    res.render('admin/users', {
-
-    });
+    res.render('admin/users', admin.getParams(req));
 
 });
 
